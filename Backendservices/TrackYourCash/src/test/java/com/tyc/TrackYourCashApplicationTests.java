06ad3a6596fb6b5307939136be0a34b55e3ae30d
Shribas Kundu <shribaskundu12@gmail.com>
2025-08-07 16:00:17 +0530

    Project monorepo created...

---
+++ Backendservices/TrackYourCash/src/test/java/com/tyc/TrackYourCashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackYourCashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
